8b54422c58ba5e2d27208ea55b30d4f70862ed31
Nahed <nahedben@gmail.com>
2020-04-05 22:23:36 +0300

    adding Angular part

--- src/main/java/com/nahed/onlinebookstore/repository/BookRepository.java
+++ src/main/java/com/nahed/onlinebookstore/repository/BookRepository.java
@@ -5,7 +5,8 @@ import com.nahed.onlinebookstore.entity.Book;
 import com.nahed.onlinebookstore.entity.BookCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-
+@CrossOrigin("*")
 public interface BookRepository  extends JpaRepository<Book,Long>{
 }
